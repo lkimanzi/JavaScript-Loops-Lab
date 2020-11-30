6e38a6ef42f26a6875ee01a35b4d9813c75bc554
Lucas Kimanzi <lkimanzi@hotmail.com>
2020-11-30 05:53:30 -0500

    1-10 for loop

---
+++ main.js
@@ -0,0 +1,8 @@
+//1. Code to Display the numbers 1 to 10 in the console.
+
+//one_to_ten() {
+ //   console.log("Using 'for' loop ");
+    for (let i = 0; i <= 10; i++) {
+      console.log(i);
+    }
+    // Above goes for iterations
