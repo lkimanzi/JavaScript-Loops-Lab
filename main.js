0e26ef3b22077ae14ebe22c0cf852cda95360911
Lucas Kimanzi <lkimanzi@hotmail.com>
2020-12-01 19:45:59 -0500

    square  loop

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 //1. Code to Display the numbers 1 to 10 in the console.
 
-//one_to_ten() {
- //   console.log("Using 'for' loop ");
+
+    
     for (let i = 0; i <= 10; i++) {
       console.log(i);
     }
-    // Above goes for iterations
+	
+	//Displays the square numbers up to 100 in the console.
+
+
+	
+    for (let i = 1; i <= 10; i++) {
+      let sqr = i * i;
+      console.log(sqr);
+      
